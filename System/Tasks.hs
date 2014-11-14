d8657dbe8613fc0a2023a0540a1b46e9fdcde845
David Fox <dsf@seereason.com>
2014-11-14 05:31:21 -0800

    Explicit exports in System.Tasks

--- System/Tasks.hs
+++ System/Tasks.hs
@@ -1,10 +1,25 @@
 module System.Tasks
-    ( module System.Tasks.Types
+    ( -- * Functions to run the Manager and IO tasks
+      manager
     , runIO
     , runProgressIO
     , MonadCancel(cancelIO, evalCancelIO, cancellable)
     , runCancelIO
-    , manager
+    -- * Task identifier class
+    , TaskId
+    -- * Task output class
+    , ProgressAndResult(taskMessage)
+    , IOPuts(..)
+    -- * Message Types by Recipient
+    , TaskTakes(..)
+    , ManagerTakes(..)
+    , TopTakes(..)
+    -- * Message Types by Sender and Recipient
+    , TaskToManager(..)
+    , ManagerToTask(..)
+    , TopToManager(..)
+    , ManagerToTop(..)
+    , ManagerStatus(..)
     ) where
 
 import System.Tasks.IO (runIO, runProgressIO, MonadCancel(cancelIO, evalCancelIO, cancellable), runCancelIO)
